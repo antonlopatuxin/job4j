f75ad23794cf7098f06eed4120e0842e211b32a0
russa <lopatuxin@mail.ru>
2017-10-11 18:41:53 +0300

    task 785 finishing

--- Tracker/src/main/java/ru/job4j/start/Menu.java
+++ Tracker/src/main/java/ru/job4j/start/Menu.java
@@ -8,6 +8,12 @@ public class Menu {
     private Tracker tracker;
     private int switchPar; // параметр свитч
     private Input input;
+    private final int addItem = 0;
+    private final int findAllItems = 1;
+    private final int updateItem = 2;
+    private final int deleteItem = 3;
+    private final int findByIdItem = 4;
+    private final int findByNameItem = 5;
 
     public Menu(Tracker tracker, Input input){
         this.tracker = tracker;
@@ -16,7 +22,7 @@ public class Menu {
 
 
     // вывод меню на экран
-    private void menu(){
+    private void menu() {
         System.out.println("0. Добавление новой заявки.");
         System.out.println("1. Показать все заявки.");
         System.out.println("2. Редактирование заявки.");
@@ -24,8 +30,77 @@ public class Menu {
         System.out.println("4. Поиск заявки по id.");
         System.out.println("5. Поиск заявки по имени.");
         System.out.println("6. Выход.");
-
     }
+
+        private void addItem()throws IOException{
+        String nameItem =  input.askString("Введите имя заявки: "); // запрашиваем имя заявки
+        String descItem = input.askString("Введите описание заявки: "); // запрашиваем описание заявки
+        String timeCreate = input.askString("Введите время создания заявки: "); // пока вводим вручную поэтому и тип стринг
+        tracker.add(new Item(nameItem, descItem, timeCreate));
+        System.out.println("Заявка добавлена и сохранена");
+        System.out.println("<===================================================>");
+    }
+
+    private void findAllItems()throws IOException{
+        if(tracker.findAll() == null){
+            System.out.println("Список заявок пуст");
+            System.out.println("<=====================================================>");
+        }else {
+            for(int count = 0; count < tracker.findAll().length; count++){
+                System.out.println("Имя заявки: " + tracker.findAll()[count].getName());
+                System.out.println("Описание заявки: " + tracker.findAll()[count].getDescription());
+                System.out.println("Время создания заявки: " + tracker.findAll()[count].getCreate());
+                System.out.println("ID заявки: " + tracker.findAll()[count].getId());
+                if (count == tracker.findAll().length - 1) {
+                    System.out.println("<=====================================================>");
+                } else {
+                    System.out.println("<------------------------------------------------------>");
+                }
+            }
+        }
+    }
+
+    private void updateItem()throws IOException{
+        String[] arrayUp = new String[10];
+        arrayUp[0] = input.askString("Введите id заявки: ");
+        Item itemUp = tracker.findById(arrayUp[0]);
+        if(itemUp.getId().equals(arrayUp[0])){
+            arrayUp[1] = input.askString("Введите новое имя заявки:");
+            arrayUp[2] = input.askString("Введите новое описание заявки:");
+            arrayUp[3] = input.askString("Введите время редактирования заявки:");
+            tracker.update(itemUp, arrayUp);
+            System.out.println("Заявка изменена и сохранена");
+            System.out.println("<===========================================================>");
+        }else{
+            System.out.println("Заявка не найдена");
+        }
+    }
+
+    private void deleteItem()throws IOException{
+        String idDelete = input.askString("Введите id заявки: ");
+        tracker.delete(idDelete);
+
+        System.out.println("Заявка удалена.");
+    }
+
+    private void findByIdItem()throws IOException{
+        String findId = input.askString("Введите id заявки: ");
+        Item item = tracker.findById(findId);
+
+        System.out.println(item.getCreate());
+        System.out.println(item.getName());
+        System.out.println(item.getDescription());
+    }
+
+    private void findByNameItem()throws IOException{
+        String name = input.askString("Введите имя заявки: ");
+        Item itemName = tracker.findByName(name);
+
+        System.out.println((itemName.getCreate()));
+        System.out.println(itemName.getId());
+        System.out.println(itemName.getDescription());
+    }
+
     // реализация работы меню
     public void menuAction() throws IOException {
 
@@ -37,75 +112,28 @@ public class Menu {
 
             switch (switchPar) {
 
-                case 0:
-                    String nameItem =  input.askString("Введите имя заявки: "); // запрашиваем имя заявки
-                    String descItem = input.askString("Введите описание заявки: "); // запрашиваем описание заявки
-                    String timeCreate = input.askString("Введите время создания заявки: "); // пока вводим вручную поэтому и тип стринг
-                    tracker.add(new Item(nameItem, descItem, timeCreate));
-                    System.out.println("Заявка добавлена и сохранена");
-                    System.out.println("<===================================================>");
+                case addItem:
+                    addItem();
                     break;
 
-                case 1:
-                    if(tracker.findAll() == null){
-                        System.out.println("Список заявок пуст");
-                        System.out.println("<=====================================================>");
-                        }else {
-                        for(int count = 0; count < tracker.findAll().length; count++){
-                            System.out.println("Имя заявки: " + tracker.findAll()[count].getName());
-                            System.out.println("Описание заявки: " + tracker.findAll()[count].getDescription());
-                            System.out.println("Время создания заявки: " + tracker.findAll()[count].getCreate());
-                            System.out.println("ID заявки: " + tracker.findAll()[count].getId());
-                            if (count == tracker.findAll().length - 1) {
-                                System.out.println("<=====================================================>");
-                            } else {
-                                System.out.println("<------------------------------------------------------>");
-                            }
-                        }
-                    }
+                case findAllItems:
+                    findAllItems();
                     break;
 
-                case 2:
-                    String[] arrayUp = new String[10];
-                    arrayUp[0] = input.askString("Введите id заявки: ");
-                    Item itemUp = tracker.findById(arrayUp[0]);
-                    if(itemUp.getId().equals(arrayUp[0])){
-                        arrayUp[1] = input.askString("Введите новое имя заявки:");
-                        arrayUp[2] = input.askString("Введите новое описание заявки:");
-                        arrayUp[3] = input.askString("Введите время редактирования заявки:");
-                        tracker.update(itemUp, arrayUp);
-                        System.out.println("Заявка изменена и сохранена");
-                        System.out.println("<===========================================================>");
-                    }else{
-                        System.out.println("Заявка не найдена");
-                    }
+                case updateItem:
+                   updateItem();
+                   break;
+
+                case deleteItem:
+                    deleteItem();
                     break;
 
-                case 3:
-                    String idDelete = input.askString("Введите id заявки: ");
-                    tracker.delete(idDelete);
-
-                    System.out.println("Заявка удалена.");
+                case findByIdItem:
+                    findByIdItem();
                     break;
 
-                case 4:
-                    String findId = input.askString("Введите id заявки: ");
-                    Item item = tracker.findById(findId);
-
-                    System.out.println(item.getCreate());
-                    System.out.println(item.getName());
-                    System.out.println(item.getDescription());
-
-                    break;
-
-                case 5:
-                    String name = input.askString("Введите имя заявки: ");
-                    Item itemName = tracker.findByName(name);
-
-                    System.out.println((itemName.getCreate()));
-                    System.out.println(itemName.getId());
-                    System.out.println(itemName.getDescription());
-
+                case findByNameItem:
+                    findByNameItem();
                     break;
 
             }
--- Tracker/src/main/java/ru/job4j/start/StartUI.java
+++ Tracker/src/main/java/ru/job4j/start/StartUI.java
@@ -1,6 +1,5 @@
 package ru.job4j.start;
 
-import com.sun.deploy.trace.Trace;
 import ru.job4j.tracker.Item;
 import ru.job4j.tracker.Tracker;
 
@@ -11,10 +10,69 @@ public class StartUI {
     private Input input;
     private Tracker tracker;
     private int switchValue;
+    private final int addItem = 0;
+    private final int findAllItems = 1;
+    private final int updateItem = 2;
+    private final int deleteItem = 3;
+    private final int findByIdItem = 4;
+    private final int findByNameItem = 5;
 
     public StartUI(Input input, Tracker tracker) {
         this.input = input;
         this.tracker = tracker;
+    }
+
+    private void addItem()throws IOException{
+        String nameItem = input.askString("Введите имя заявки: "); // запрашиваем имя заявки
+        String descItem = input.askString("Введите описание заявки: "); // запрашиваем описание заявки
+        String timeCreate = input.askString("Введите время создания заявки: "); // пока вводим вручную поэтому и тип стринг
+        tracker.add(new Item(nameItem, descItem, timeCreate));
+    }
+
+    private void findAllItems()throws IOException{
+        for(int count = 0; count < tracker.findAll().length; count++){
+        System.out.println("Имя заявки: " + tracker.findAll()[count].getName());
+        System.out.println("Описание заявки: " + tracker.findAll()[count].getDescription());
+        System.out.println("Время создания заявки: " + tracker.findAll()[count].getCreate());
+        System.out.println("ID заявки: " + tracker.findAll()[count].getId());
+        if(count == tracker.findAll().length - 1){
+            System.out.println("<==================================================>");
+        }else{
+            System.out.println("<-------------------------------------------------->");
+        }
+
+        }
+    }
+
+    private void updateItems()throws IOException{
+        String[] arrayUpdate = new String[10];
+        arrayUpdate[0] = input.askString("Введите id заявки: ");
+        Item itemUpdate = tracker.findById(arrayUpdate[0]);
+        if(itemUpdate.getId().equals(arrayUpdate[0])){
+            arrayUpdate[1] = input.askString("Введите новое имя заявки:");
+            arrayUpdate[2] = input.askString("Введите новое описание заявки:");
+            arrayUpdate[3] = input.askString("Введите время редактирования заявки:");
+            tracker.update(itemUpdate, arrayUpdate);
+            System.out.println("Заявка изменена и сохранена ");
+            System.out.println("<==================================================>");
+        }else{
+            System.out.println("Заявка не найдена");
+        }
+    }
+
+    private void deleteItem()throws IOException{
+        String idDelete = input.askString("Введите id заявки: ");
+        tracker.delete(idDelete);
+    }
+
+    private void findByIdItem()throws IOException{
+        String findId = input.askString("Введите id заявки: ");
+        tracker.findById(findId);
+    }
+
+    private void findByNameItem()throws IOException{
+        String name = input.askString("Введите имя заявки: ");
+        tracker.findByName(name);
     }
 
     public void init() throws IOException {
@@ -25,59 +83,28 @@ public class StartUI {
 
             switch(switchValue){
 
-                case 0:
-                    String nameItem =  input.askString("Введите имя заявки: "); // запрашиваем имя заявки
-                    String descItem = input.askString("Введите описание заявки: "); // запрашиваем описание заявки
-                    String timeCreate = input.askString("Введите время создания заявки: "); // пока вводим вручную поэтому и тип стринг
-                    tracker.add(new Item(nameItem, descItem, timeCreate));
+                case addItem:
+                    addItem();
                     break;
 
-                case 1:
-                    for(int count = 0; count < tracker.findAll().length; count++){
-                        System.out.println("Имя заявки: " + tracker.findAll()[count].getName());
-                        System.out.println("Описание заявки: " + tracker.findAll()[count].getDescription());
-                        System.out.println("Время создания заявки: " + tracker.findAll()[count].getCreate());
-                        System.out.println("ID заявки: " + tracker.findAll()[count].getId());
-                       if(count == tracker.findAll().length - 1){
-                           System.out.println("<==================================================>");
-                       }else{
-                           System.out.println("<-------------------------------------------------->");
-                       }
-                    }
+                case findAllItems:
+                    findAllItems();
                     break;
 
-                case 2:
-                    String[] arrayUpdate = new String[10];
-                    arrayUpdate[0] = input.askString("Введите id заявки: ");
-                    Item itemUpdate = tracker.findById(arrayUpdate[0]);
-                    if(itemUpdate.getId().equals(arrayUpdate[0])){
-                        arrayUpdate[1] = input.askString("Введите новое имя заявки:");
-                        arrayUpdate[2] = input.askString("Введите новое описание заявки:");
-                        arrayUpdate[3] = input.askString("Введите время редактирования заявки:");
-                        tracker.update(itemUpdate, arrayUpdate);
-                        System.out.println("Заявка изменена и сохранена ");
-                        System.out.println("<==================================================>");
-                    }else{
-                        System.out.println("Заявка не найдена");
-                    }
+                case updateItem:
+                    updateItems();
                     break;
 
-                case 3:
-                    String idDelete = input.askString("Введите id заявки: ");
-                    tracker.delete(idDelete);
-
+                case deleteItem:
+                    deleteItem();
                     break;
 
-                case 4:
-                    String findId = input.askString("Введите id заявки: ");
-                    tracker.findById(findId);
-
+                case findByIdItem:
+                    findByIdItem();
                     break;
 
-                case 5:
-                    String name = input.askString("Введите имя заявки: ");
-                    tracker.findByName(name);
-
+                case findByNameItem:
+                    findByNameItem();
                     break;
             }
         }
